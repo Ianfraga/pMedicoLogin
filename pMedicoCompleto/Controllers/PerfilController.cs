d35a74d354e1896c37758146225b08f98d07cc89
Senai <ian-fraga@hotmail.com>
2019-04-22 13:48:18 -0300

    Projeto Final MeuMédico com Login

---
+++ pMedicoCompleto/Controllers/PerfilController.cs
@@ -0,0 +1,46 @@
+﻿using pMedicoCompleto.DAO;
+using pMedicoCompleto.Utils;
+using pMedicoCompleto.ViewModels;
+using System.Linq;
+using System.Security.Claims;
+using System.Web.Mvc;
+
+namespace pMedicoCompleto.Controllers
+{
+    public class PerfilController : Controller
+    {
+        private EFContext db = new EFContext();
+        // GET: Perfil
+        [Authorize]
+        public ActionResult AlterarSenha()
+        {
+            return View();
+            
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult AlterarSenha(AlterarSenhaViewModels viewmodel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var identity = User.Identity as ClaimsIdentity;
+            var login = identity.Claims.FirstOrDefault(c => c.Type == "Login").Value;
+            var usuario = db.Usuarios.FirstOrDefault(u => u.Login == login);
+
+            if (Hash.gerarHash(viewmodel.senhaAtual) != usuario.Senha)
+            {
+                ModelState.AddModelError("senhaAtual", "Senha Incorreta");
+                return View();
+            }
+            usuario.Senha = Hash.gerarHash(viewmodel.senhaNova);
+            db.Entry(usuario).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Painel");
+
+        }
+    }
+}
---
+++ pMedicoCompleto/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace pMedicoCompleto.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<pMedicoCompleto.DAO.EFContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(pMedicoCompleto.DAO.EFContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
